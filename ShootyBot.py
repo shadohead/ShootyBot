d2ce4b3eff22b3f98abd08da067da4aa86f564a7
Shadohead <shadohead@gmail.com>
2022-01-13 00:32:59 -0800

    Change commands from  to  to resolve conflict with Mudae

--- ShootyBot.py
+++ ShootyBot.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.INFO)
 pp = pprint.PrettyPrinter(indent=4)
 
 # set bot token here to run
-BOT_TOKEN = 'OTMxMDgxNjUzNjE3NTc4MDA3.Yd_PXA.c5EEXeRvP_qXEMzLVQUXBFchMzo'
+BOT_TOKEN = ''
 DEFAULT_MSG = "<@&773770148070424657>"  # shooty role code
 
 intents = discord.Intents.default()
@@ -18,9 +18,10 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='!!', intents=intents)
 
-user_set = set() # set with all the users in the shooty crew
-user_5_set = set() # set with all the 5 stack only users in the shooty crew
+user_set = set()  # set with all the users in the shooty crew
+user_5_set = set()  # set with all the 5 stack only users in the shooty crew
 latest_bot_message_time = datetime.now()
+
 
 @bot.event
 async def on_ready():
@@ -31,34 +32,34 @@ async def on_ready():
 async def on_message(message):
     global latest_bot_message_time
 
-    # ensure bot only adds reaction emojis to messages by itself and containing the default message 
+    # ensure bot only adds reaction emojis to messages by itself and containing the default message
     if message.author == bot.user and message.content.startswith(DEFAULT_MSG):
         latest_bot_message_time = message.created_at
         await message.add_reaction('\N{THUMBS UP SIGN}')
         await message.add_reaction('5️⃣')
 
     # default mode: create new session
-    if message.content == ('$shooty') or message.content == ('$s'):
+    if message.content == ('$shooty') or message.content == ('$st'):
         user_set.clear
         user_5_set.clear
 
         await message.channel.send(DEFAULT_MSG)
 
     # display status
-    elif message.content == ('$shooty status') or message.content == ('$ss'):
+    elif message.content == ('$shooty status') or message.content == ('$sts'):
         await message.channel.send(party_status_message(user_set, user_5_set))
 
     # clear the user sets
-    elif message.content == ('$shooty clear') or message.content == ('$sc'):
+    elif message.content == ('$shooty clear') or message.content == ('$stc'):
         user_set.clear
         user_5_set.clear
 
     # mention all reactors
-    elif message.content == ('$shooty mention') or message.content == ('$sm'):
+    elif message.content == ('$shooty mention') or message.content == ('$stm'):
         await mention_reactors(message)
 
     # display help message
-    elif message.content.startswith('$shooty') or message.content.startswith('$s'):
+    elif message.content.startswith('$shooty') or message.content.startswith('$st'):
         await message.channel.send(help_message())
 
 
@@ -116,7 +117,7 @@ async def on_reaction_remove(reaction, user):
         logging.info("Removed [" + user.name + "] from stack.")
         logging.info("stack:" + str(names_list(user_set)))
 
-        # case: user removed themselves from the regular group, but still has a react for the 5 stack 
+        # case: user removed themselves from the regular group, but still has a react for the 5 stack
         # check if user has a 5 stack emoji react, if so add them back to that set
         if user in await reaction.message.reactions[1].users().flatten():
             user_5_set.add(user)
@@ -138,7 +139,9 @@ async def on_reaction_remove(reaction, user):
 
         await reaction.message.edit(content=new_message)
 
-#Sends message mentioning everyone in the shooty crew
+# Sends message mentioning everyone in the shooty crew
+
+
 async def mention_reactors(message):
     if not user_set and not user_5_set:
         await message.channel.send("Nobody to mention here.")
@@ -154,32 +157,42 @@ async def mention_reactors(message):
     await message.channel.send(mention_message)
 
 
-#String formatted with status of the shooty crew
+# String formatted with status of the shooty crew
 def party_status_message(player_set, five_stack_set):
 
     if len(player_set)+len(five_stack_set) >= 5:
         new_message = DEFAULT_MSG + "\n\n"\
             + bold(str(len(player_set)+len(five_stack_set))) + bold("/5")\
-            + "\n" + pretty_player_sets(names_list(player_set), names_list(five_stack_set))
+            + "\n" + \
+            pretty_player_sets(names_list(player_set),
+                               names_list(five_stack_set))
     elif player_set and five_stack_set:
         new_message = DEFAULT_MSG + "\n\n"\
             + bold(str(len(player_set))) + "(" + str(len(five_stack_set)) + ")" + bold("/5")\
-            + "\n" + pretty_player_sets(names_list(player_set), names_list(five_stack_set))
+            + "\n" + \
+            pretty_player_sets(names_list(player_set),
+                               names_list(five_stack_set))
     elif player_set:
         new_message = DEFAULT_MSG + "\n\n"\
             + bold(str(len(player_set)) + "/5")\
-            + "\n" + pretty_player_sets(names_list(player_set), names_list(five_stack_set))
+            + "\n" + \
+            pretty_player_sets(names_list(player_set),
+                               names_list(five_stack_set))
     elif five_stack_set:
         new_message = DEFAULT_MSG + "\n\n"\
             + "(" + str(len(five_stack_set)) + ")" + bold("/5")\
-            + "\n" + pretty_player_sets(names_list(player_set), names_list(five_stack_set))
+            + "\n" + \
+            pretty_player_sets(names_list(player_set),
+                               names_list(five_stack_set))
     else:
         new_message = "" + DEFAULT_MSG + "\n\n"\
             + "sadge/5"
 
     return new_message
 
-#Returns pretty print formatted player sets
+# Returns pretty print formatted player sets
+
+
 def pretty_player_sets(player_set, five_stack_set):
     result_string = ''
 
@@ -199,7 +212,9 @@ def pretty_player_sets(player_set, five_stack_set):
 
     return result_string
 
-#Returns list of usernames(str) from input of a User collection
+# Returns list of usernames(str) from input of a User collection
+
+
 def names_list(users):
     result_list = []
     for user in users:
@@ -217,10 +232,10 @@ def italics(input):
 
 def help_message():
     return "ShootyBot help:" + "\n\n"\
-        + "*$shooty* or *$s* -- Starts new Shooty session \n" \
-        + "*$shooty status* or *$ss* -- Shows current Shooty session status \n"\
-        + "*$shooty mention* or *$sm* -- Mentions all session members (5 stackers included)\n"\
-        + "*$shooty clear* or *$sc* -- Clears current Shooty session"
+        + "*$shooty* or *$st* -- Starts new Shooty session \n" \
+        + "*$shooty status* or *$sts* -- Shows current Shooty session status \n"\
+        + "*$shooty mention* or *$stm* -- Mentions all session members (5 stackers included)\n"\
+        + "*$shooty clear* or *$stc* -- Clears current Shooty session"
 
 
 bot.run(BOT_TOKEN)
