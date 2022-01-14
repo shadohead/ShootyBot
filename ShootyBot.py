d898069e3675a4d89d36f6a2343702d3fe7f4668
Shadohead <shadohead@gmail.com>
2022-01-14 00:25:07 -0800

    set custom party size and refactor variable names to hurt less

--- ShootyBot.py
+++ ShootyBot.py
@@ -10,20 +10,21 @@ logging.basicConfig(level=logging.INFO)
 pp = pprint.PrettyPrinter(indent=4)
 
 # set bot token here to run
-BOT_TOKEN = ''
+BOT_TOKEN = 'OTMxMDgxNjUzNjE3NTc4MDA3.Yd_PXA.mFNGsYewecvGii4fm0A8BwkFoaI'
 DEFAULT_MSG = "‎"  # Invisible character magic (this is terrible lol)
 SHOOTY_ROLE_CODE = "<@&773770148070424657>"
+DEFAULT_PARTY_SIZE = 5
 
 intents = discord.Intents.default()
 intents.members = True
 
 bot = commands.Bot(command_prefix='!!', intents=intents)
 
-user_set = set()  # set with all the users in the shooty crew
-user_5_set = set()  # set with all the 5 stack only users in the shooty crew
-ready_set = set()  # set with all players who said they're ready to play right now
+bot_soloq_user_set = set()  # set with all the users in the shooty crew
+bot_fullstack_user_set = set()  # set with all the full stack only users in the shooty crew
+bot_ready_user_set = set()  # set with all players who said they're ready to play right now
 latest_bot_message_time = datetime.now()
-
+party_max_size = DEFAULT_PARTY_SIZE
 
 @bot.event
 async def on_ready():
@@ -33,6 +34,7 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     global latest_bot_message_time
+    global party_max_size
 
     # ensure bot only adds reaction emojis to messages by itself and containing the default message
     if message.author == bot.user and message.content.startswith(DEFAULT_MSG):
@@ -44,22 +46,24 @@ async def on_message(message):
     # default mode: create new session
     if message.content == ('$shooty') or message.content == ('$st'):
         logging.info("Starting new shooty session")
-        user_set.clear()
-        user_5_set.clear()
+        bot_soloq_user_set.clear()
+        bot_fullstack_user_set.clear()
+        bot_ready_user_set.clear()
 
         await message.channel.send(DEFAULT_MSG+SHOOTY_ROLE_CODE)
 
     # display status
     elif message.content == ('$shooty status') or message.content == ('$sts'):
         logging.info("Printing Status")
-        await message.channel.send(party_status_message(user_set, user_5_set, True))
+        await message.channel.send(party_status_message(bot_soloq_user_set, bot_fullstack_user_set, True))
 
     # clear the user sets
     elif message.content == ('$shooty clear') or message.content == ('$stc'):
         logging.info("Clearing user sets: " +
-                     str(to_names_list(user_set.union(user_5_set))))
-        user_set.clear()
-        user_5_set.clear()
+                     str(to_names_list(bot_soloq_user_set.union(bot_fullstack_user_set))))
+        bot_soloq_user_set.clear()
+        bot_fullstack_user_set.clear()
+        bot_ready_user_set.clear()
 
         await message.channel.send("Cleared shooty session.")
 
@@ -70,38 +74,47 @@ async def on_message(message):
     # remove the person from both groups
     elif message.content.startswith('$shooty kick'):
         split_message = message.content.split()
-        kick_users_names = split_message[2:]
-        actually_kicked_usernames = []
-        for username in kick_users_names:
-            for user in user_set.copy():
+        user_names_list = split_message[2:]
+        kicked_usernames_list = []
+        for username in user_names_list:
+            for user in bot_soloq_user_set.copy():
                 if user.name.startswith(username):
-                    user_set.remove(user)
-                    actually_kicked_usernames.append(user.name)
+                    bot_soloq_user_set.remove(user)
+                    kicked_usernames_list.append(user.name)
 
-            for user in user_5_set.copy():
+            for user in bot_fullstack_user_set.copy():
                 if user.name.startswith(username):
-                    user_5_set.remove(user)
-                    actually_kicked_usernames.append(user.name)
+                    bot_fullstack_user_set.remove(user)
+                    kicked_usernames_list.append(user.name)
 
-        await message.channel.send("Kicked: " + str(actually_kicked_usernames))
+        await message.channel.send("Kicked: " + str(kicked_usernames_list))
 
     elif message.content.startswith('$stk'):
         split_message = message.content.split()
-        kick_users_names = split_message[1:]
-        actually_kicked_usernames = []
+        user_names_list = split_message[1:]
+        kicked_usernames_list = []
 
-        for username in kick_users_names:
-            for user in user_set.copy():
+        for username in user_names_list:
+            for user in bot_soloq_user_set.copy():
                 if user.name.startswith(username):
-                    user_set.remove(user)
-                    actually_kicked_usernames.append(user.name)
+                    bot_soloq_user_set.remove(user)
+                    kicked_usernames_list.append(user.name)
 
-            for user in user_5_set.copy():
+            for user in bot_fullstack_user_set.copy():
                 if user.name.startswith(username):
-                    user_5_set.remove(user)
-                    actually_kicked_usernames.append(user.name)
+                    bot_fullstack_user_set.remove(user)
+                    kicked_usernames_list.append(user.name)
 
-        await message.channel.send("Kicked: " + str(actually_kicked_usernames))
+        await message.channel.send("Kicked: " + str(kicked_usernames_list))
+    
+    elif message.content.startswith('$shooty size'):
+        split_message = message.content.split()
+
+        if len(split_message) >= 3 and split_message[2].isdigit():
+            new_party_size = split_message[2]
+            party_max_size = int(new_party_size)
+        else:
+            await message.channel.send("Current party size: " + str(party_max_size))
 
     # display help message
     elif message.content.startswith('$shooty') or message.content.startswith('$st'):
@@ -120,36 +133,36 @@ async def on_reaction_add(reaction, user):
 
     # handle main group of players
     if reaction.emoji == '\N{THUMBS UP SIGN}':
-        # case: user was in the 5 stack only group and wanted to join the regular group
-        # remove them from 5 stack group and put them in the regular group instead
-        if user in user_5_set:
-            user_5_set.remove(user)
+        # case: user was in the full stack only group and wanted to join the regular group
+        # remove them from full stack group and put them in the regular group instead
+        if user in bot_fullstack_user_set:
+            bot_fullstack_user_set.remove(user)
 
-        user_set.add(user)
-        logging.info("stack:" + str(to_names_list(user_set)))
+        bot_soloq_user_set.add(user)
+        logging.info("stack:" + str(to_names_list(bot_soloq_user_set)))
 
         new_message = party_status_message(
-            user_set, user_5_set, True)
+            bot_soloq_user_set, bot_fullstack_user_set, True)
 
         await reaction.message.edit(content=new_message)
 
-    # handle 5 stack only players
+    # handle full stack only players
     elif reaction.emoji == '5️⃣':
-        if user not in user_set:
-            user_5_set.add(user)
-            logging.info("5stack:" + str(to_names_list(user_5_set)))
+        if user not in bot_soloq_user_set:
+            bot_fullstack_user_set.add(user)
+            logging.info("fullstack:" + str(to_names_list(bot_fullstack_user_set)))
 
         new_message = party_status_message(
-            user_set, user_5_set, True)
+            bot_soloq_user_set, bot_fullstack_user_set, True)
 
         await reaction.message.edit(content=new_message)
 
     elif reaction.emoji == '✅':
-        ready_set.add(user)
-        logging.info("ready_set:" + str(to_names_list(ready_set)))
+        bot_ready_user_set.add(user)
+        logging.info("ready_set:" + str(to_names_list(bot_ready_user_set)))
 
         new_message = party_status_message(
-            user_set, user_5_set, True)
+            bot_soloq_user_set, bot_fullstack_user_set, True)
 
         await reaction.message.edit(content=new_message)
 
@@ -165,40 +178,40 @@ async def on_reaction_remove(reaction, user):
         return
 
     # handle main group of players
-    if reaction.emoji == '\N{THUMBS UP SIGN}' and user in user_set:
-        user_set.remove(user)
+    if reaction.emoji == '\N{THUMBS UP SIGN}' and user in bot_soloq_user_set:
+        bot_soloq_user_set.remove(user)
 
         logging.info("Removed [" + user.name + "] from stack.")
-        logging.info("stack:" + str(to_names_list(user_set)))
+        logging.info("stack:" + str(to_names_list(bot_soloq_user_set)))
 
-        # case: user removed themselves from the regular group, but still has a react for the 5 stack
-        # check if user has a 5 stack emoji react, if so add them back to that set
+        # case: user removed themselves from the regular group, but still has a react for the full stack
+        # check if user has a full stack emoji react, if so add them back to that set
         if user in await reaction.message.reactions[1].users().flatten():
-            user_5_set.add(user)
-            logging.info("5stack:" + str(to_names_list(user_set)))
+            bot_fullstack_user_set.add(user)
+            logging.info("fullstack:" + str(to_names_list(bot_soloq_user_set)))
 
         new_message = party_status_message(
-            user_set, user_5_set, True)
+            bot_soloq_user_set, bot_fullstack_user_set, True)
 
         await reaction.message.edit(content=new_message)
 
-    # handle 5 stack only players
-    elif reaction.emoji == '5️⃣' and user in user_5_set:
-        user_5_set.remove(user)
-        logging.info("Removed [" + user.name + "] from 5 stack.")
-        logging.info("5stack:" + str(to_names_list(user_5_set)))
+    # handle full stack only players
+    elif reaction.emoji == '5️⃣' and user in bot_fullstack_user_set:
+        bot_fullstack_user_set.remove(user)
+        logging.info("Removed [" + user.name + "] from full stack.")
+        logging.info("fullstack:" + str(to_names_list(bot_fullstack_user_set)))
 
         new_message = party_status_message(
-            user_set, user_5_set, True)
+            bot_soloq_user_set, bot_fullstack_user_set, True)
 
         await reaction.message.edit(content=new_message)
 
     elif reaction.emoji == '✅':
-        ready_set.remove(user)
-        logging.info("ready_set:" + str(to_names_list(ready_set)))
+        bot_ready_user_set.remove(user)
+        logging.info("ready_set:" + str(to_names_list(bot_ready_user_set)))
 
         new_message = party_status_message(
-            user_set, user_5_set, True)
+            bot_soloq_user_set, bot_fullstack_user_set, True)
 
         await reaction.message.edit(content=new_message)
 
@@ -206,14 +219,12 @@ async def on_reaction_remove(reaction, user):
 
 
 async def mention_reactors(message):
-    if not user_set and not user_5_set:
+    if not bot_soloq_user_set and not bot_fullstack_user_set:
         await message.channel.send("No shooty boys to mention.")
         return
 
     mention_message = ''
-    regular_stack_reactors = user_set
-    five_stack_reactors = user_5_set
-    for user in regular_stack_reactors.union(five_stack_reactors):
+    for user in bot_soloq_user_set.union(bot_fullstack_user_set):
         if not user.bot:
             mention_message += user.mention + " "
 
@@ -221,61 +232,61 @@ async def mention_reactors(message):
 
 
 # String formatted with status of the shooty crew
-def party_status_message(player_set, five_stack_set, isPing):
-    num_players = len(player_set)
-    num_5players = len(five_stack_set)
+def party_status_message(soloq_users_set, fullstack_users_set, isPing):
+    num_soloq_players = len(soloq_users_set)
+    num_fullstack_players = len(fullstack_users_set)
 
     if isPing:
         msg = DEFAULT_MSG + SHOOTY_ROLE_CODE
     else:
         msg = DEFAULT_MSG
 
-    if num_players+num_5players >= 5:
+    if num_soloq_players+num_fullstack_players >= party_max_size:
         new_message = msg + "\n\n"\
-            + bold(str(num_players+num_5players) + "/5")\
+            + bold(str(num_soloq_players+num_fullstack_players) + "/" + str(party_max_size))\
             + "\n" + \
-            pretty_player_sets(player_set, five_stack_set)
-    elif player_set and five_stack_set:
+            pretty_player_sets(soloq_users_set, fullstack_users_set)
+    elif soloq_users_set and fullstack_users_set:
         new_message = msg + "\n\n"\
-            + bold(str(num_players)) + "(" + str(num_players+num_5players) + ")" + bold("/5")\
+            + bold(str(num_soloq_players)) + "(" + str(num_soloq_players+num_fullstack_players) + ")" + bold("/" + str(party_max_size))\
             + "\n" + \
-            pretty_player_sets(player_set, five_stack_set)
+            pretty_player_sets(soloq_users_set, fullstack_users_set)
 
-    elif player_set:
+    elif soloq_users_set:
         new_message = msg + "\n\n"\
-            + bold(str(num_players) + "/5")\
+            + bold(str(num_soloq_players) + "/" + str(party_max_size))\
             + "\n" + \
-            pretty_player_sets(player_set, five_stack_set)
+            pretty_player_sets(soloq_users_set, fullstack_users_set)
 
-    elif five_stack_set:
+    elif fullstack_users_set:
         new_message = msg + "\n\n"\
-            + "(" + str(num_5players) + ")" + bold("/5")\
+            + "(" + str(num_fullstack_players) + ")" + bold("/" + str(party_max_size))\
             + "\n" + \
-            pretty_player_sets(player_set, five_stack_set)
+            pretty_player_sets(soloq_users_set, fullstack_users_set)
     else:
         new_message = "" + msg + "\n\n"\
-            + "sadge/5"
+            + "sadge/" + str(party_max_size)
 
     return new_message
 
 # Returns pretty print formatted player sets with input names list
 
 
-def pretty_player_sets(player_set, five_stack_set):
+def pretty_player_sets(soloq_users_set, fullstack_users_set):
     result_string = ''
 
-    for index, player_user in enumerate(player_set):
-        result_string += bold_ready_user(player_user)
+    for index, user in enumerate(soloq_users_set):
+        result_string += bold_readied_user(user)
 
         # if it's not the last player, add a comma
-        if index < len(player_set) + len(five_stack_set) - 1:
+        if index < len(soloq_users_set) + len(fullstack_users_set) - 1:
             result_string += ", "
 
-    for index, player_user in enumerate(five_stack_set):
-        result_string += italics(bold_ready_user(player_user))
+    for index, user in enumerate(fullstack_users_set):
+        result_string += italics(bold_readied_user(user))
 
         # if it's not the last player, add a comma
-        if index < len(five_stack_set) - 1:
+        if index < len(fullstack_users_set) - 1:
             result_string += ", "
 
     return result_string
@@ -283,36 +294,37 @@ def pretty_player_sets(player_set, five_stack_set):
 # Returns list of usernames(str) from input of a User collection
 
 
-def to_names_list(users):
+def to_names_list(user_set):
     result_list = []
-    for user in users:
+    for user in user_set:
         result_list.append(user.name)
     return result_list
 
 
-def bold(input):
-    return "**" + input + "**"
+def bold(input_str):
+    return "**" + input_str + "**"
 
 # Returns bold username if the user is ready
 
 
-def bold_ready_user(input_user):
-    if input_user in ready_set:
-        return "**" + input_user.name + "**"
+def bold_readied_user(user):
+    if user in bot_ready_user_set:
+        return "**" + user.name + "**"
     else:
-        return input_user.name
+        return user.name
 
 
-def italics(input):
-    return "*" + input + "*"
+def italics(input_str):
+    return "*" + input_str + "*"
 
 
 def help_message():
     return "ShootyBot help:" + "\n\n"\
         + "*$shooty* or *$st* -- Starts new Shooty session \n" \
         + "*$shooty status* or *$sts* -- Shows current Shooty session status \n"\
-        + "*$shooty mention* or *$stm* -- Mentions all session members (5 stackers included)\n"\
-        + "*$shooty kick* or *$stk* -- Kick the shooter(s) from session\n"\
+        + "*$shooty mention* or *$stm* -- Mentions all session members (full stackers included)\n"\
+        + "*$shooty kick user1 ...* or *$stk user1 ...* -- Kick the shooter(s) from session\n"\
+        + "*$shooty size N*  -- Set the max party size\n"\
         + "*$shooty clear* or *$stc* -- Clears current Shooty session"
 
 
