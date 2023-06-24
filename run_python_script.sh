1dbe55c9449168ed0cb7e15f99aab13a724ead4d
Shadohead <shadohead@gmail.com>
2023-06-23 23:26:10 -0700

    AutoRun script for cronjob
    * * * * * /bin/bash -l /home/pi/ShootyBot/ShootyBot/run_python_script.sh

---
+++ run_python_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# The name of the screen session
+SCREEN_NAME="shooty"
+
+
+# Source the .bashrc or .bash_profile file
+source /home/pi/.bashrc
+
+# Check if the screen session is already running
+/usr/bin/screen -list | /bin/grep -q "${SCREEN_NAME}"
+
+# If the screen session is not running, start it
+if [ $? -eq 1 ]; then
+    /usr/bin/screen -dmS ${SCREEN_NAME} /usr/bin/python3 /home/pi/ShootyBot/ShootyBot/ShootyBot.py
+fi
