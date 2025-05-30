3cc9ff36fdab7a7b7d6ba92b0aa3d4c20f241543
Ming Yue <shadohead@gmail.com>
2025-05-30 02:20:16 -0700

    fix run script

--- run_python_script.sh
+++ run_python_script.sh
@@ -3,14 +3,14 @@
 # The name of the screen session
 SCREEN_NAME="shooty"
 
-
-# Source the .bashrc or .bash_profile file
-source /home/pi/.bashrc
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Check if the screen session is already running
-/usr/bin/screen -list | /bin/grep -q "${SCREEN_NAME}"
+screen -list | grep -q "${SCREEN_NAME}"
 
 # If the screen session is not running, start it
 if [ $? -eq 1 ]; then
-    /usr/bin/screen -dmS ${SCREEN_NAME} /usr/bin/python3 /home/pi/ShootyBot/ShootyBot/bot.py
+    cd "${SCRIPT_DIR}"
+    screen -dmS ${SCREEN_NAME} ./run.sh
 fi
